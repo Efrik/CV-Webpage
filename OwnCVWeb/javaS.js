8cbcd6b7b139a918303f11561f71c22ea6e366b3
Erik Ramon Mazon <31285953+Efrik@users.noreply.github.com>
2018-07-06 16:09:35 +0200

    Add files via upload

--- OwnCVWeb/javaS.js
+++ OwnCVWeb/javaS.js
@@ -3,16 +3,27 @@ var actualSection=0;
 var oriX=0
 var objX=screen.width;
 
+//adjusting the footer
+var foot= document.getElementById("foot");
+foot.style.width=screen.width;
+//foot.style.bottom=-1400;
+
 //Making the sidescrollbox bigger
 var sidebox = document.getElementById("sidescroll");
-sidescroll.style.width=screen.width*2;
+sidebox.style.width=screen.width*2;
 
+//adjusting pagesections
 var pagesections=document.getElementsByClassName("pagesection");
-for(i=0;i<pagesections.length;i++){
+for(var i=0;i<pagesections.length;i++){
     pagesections[i].style.width=screen.width;
     pagesections[i].style.left=screen.width;
 }
 pagesections[0].style.left=0;
+
+//adjust rectitem
+var rect= document.getElementById("listrect");
+rect.style.left=50;
+var rectXO=50;
 
 function sectionLeft() {
     //alert("You just pressed left");
@@ -41,8 +52,14 @@ function sectionmove(objective){
     var section= sections[actualSection];
     var originsec= document.getElementById(section);
     var objectivesec=document.getElementById(sections[objective]);
-    //alert("section= "+section+"  objective= "+objective);
-
+    //alert("section= "+section+"  objective= "+objective);  
+    
+    //rectangle info;
+    var rectXF=50+objective*52;
+    //alert(rectXF);
+    var rectSpeed=(rectXF-rectXO)/100;
+    //alert(rectSpeed);
+    
     var moving = setInterval(move,10);
     function move(){
         if (objectivesec!==originsec){
@@ -54,13 +71,17 @@ function sectionmove(objective){
                 oriX=0;
                 objX=screen.width;
                 actualSection=objective;
+                rect.style.left=rectXF;
+                rectXO=rectXF;
             }else{
                 oriX-=20;
                 objX-=20;
+                rectXO=rectXO+rectSpeed;
                 objectivesec.style.left=objX;
                 objectivesec.style.right=oriX;
                 originsec.style.left=oriX;
                 originsec.style.right=objX;
+                rect.style.left=rectXO;
             }
         }
     }
